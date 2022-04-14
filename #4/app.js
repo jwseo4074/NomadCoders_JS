e78d99fab57ed0c767443db366457112def19b5c
zwon2056 <zwon2056@gmail.com>
2022-04-14 16:17:05 +0900

    #4.3 concept of event and event.preventDefault

--- #4/app.js
+++ #4/app.js
@@ -10,6 +10,8 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 // const loginButton = document.querySelector("#login-form button");
 
+const link = document.querySelector("a");
+
 function onLoginBtnClick() {
     const username = loginInput.value;
     console.log(username);
@@ -64,3 +66,27 @@ loginForm.addEventListener("submit", onLoginSubmit);
 // addEventListener 의 첫번째 argument는 항상 지금 막 벌어진 일들에 대한 정보이다.
 // 받는 함수에서 인자로 적어주기만 하면 됨 , 보내는건 코드에 적어놓지 않아도 JS가알아서 해줌
 
+function handleLinkClick(event) {
+    console.log(event);
+    event.preventDefault();
+    // 링크를 눌러도 페이지 이동 하지 않음, 왜 ? 
+    // 링크의 Js 기본 동작인 링크 누르면 웹 페이지 이동하는 기능(기본기능)을 막고 있거든
+    
+    // alert("click!");
+    // 이 경고창이 웹사이트가 다른 행동을 하는걸 막고 있다. (Blocking)
+    // Js가 모든걸 Stop
+
+    // 이게 이벤트를 활용하는 방법이고, preventDefault를 사용하는 방법
+}
+link.addEventListener("click", handleLinkClick);
+
+// handleLinkClick() => 내가 함수를 실행하는거야 
+// 근데 우린 이걸 원하지 않아, 클릭을 했을 때 JS가 실행을 시켜주는걸 원해
+
+handleLinkClick({})
+// 괄호(객체)를 안에 넣어주게 되면 함수를 실행 시키는 동시에 첫번째 인자로
+// 객체를 전달, 이 객체에는 많은 정보가 들어있음 (방금 일어난 event에 대한 정보들)
+// { information : about the event that just happend }
+ 
+// JS가 자동으로 객체를 전달해줄거야, 우리는 해당 함수에서 자리(공간, 파라미터를 받을 준비)만
+// 만들어주면 돼
