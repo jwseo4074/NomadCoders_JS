c09e7ed51cc1c9eabf047b044a6d1a3121e3b27e
zwon2056 <zwon2056@gmail.com>
2022-04-21 17:07:27 +0900

    Refactoring css style in First page

--- Todo_List/background.js
+++ Todo_List/background.js
@@ -1,7 +1,8 @@
 const images = [
-    "1.jpg","2.jpg","3.jpg", "4.jpg", "5.jpg",
-    "6.jpg","7.jpg","8.jpg", "9.jpg", "10.jpg",
-    "11.jpg","12.jpg","13.jpg", "14.jpg",
+    // "1.jpg","2.jpg","3.jpg", "4.jpg", "5.jpg",
+    // "6.jpg","7.jpg","8.jpg", "9.jpg", "10.jpg",
+    // "11.jpg","12.jpg","13.jpg", "14.jpg",
+    "2.jpg"
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
@@ -11,7 +12,12 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const mainForm = document.querySelector(".main_form");
 
-mainForm.style.background = `url(./img/${chosenImage})`;
+mainForm.style.background = `
+    url(./img/${chosenImage}) 
+    no-repeat 
+    0px 
+    0px
+    `;
 // style.background = "#f3f3f3 url('img_tree.png') no-repeat right top";
 
 // bgImage.classList.add("backgroundImg");
--- Todo_List/greetings.js
+++ Todo_List/greetings.js
@@ -3,11 +3,14 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const SHOW_CLASSNAME = "login_box";
+
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    loginForm.classList.remove(SHOW_CLASSNAME);
 
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
@@ -26,5 +29,6 @@ if (saveUsername === null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGreetings(saveUsername);    
+    paintGreetings(saveUsername);   
+    loginForm.classList.remove(SHOW_CLASSNAME); 
 }
